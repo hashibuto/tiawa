f2ccafd7bd95a67defe32648fbecdd3552bdb104
cowman <cowman@noreply.com>
2018-11-13 12:26:32 +0900

    bypass incompatible error instances

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 module.exports = (e) => {
+  if (!(e instanceof Error)) {
+    throw e;
+  }
+
   const newError = new Error(e.message);
-  newError.__proto__.name = e.__proto__.name;
   newError.stack = [
     ...e.stack.split('\n'),
     ...newError.stack.split('\n').slice(2)
